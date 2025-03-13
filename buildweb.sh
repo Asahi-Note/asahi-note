a6755cd38e9d845efad17a960fca3ac2a5d21ea0
Jon_Chiu <Jon_Chiu@genesis.com.tw>
2025-03-13 15:56:43 +0800

    demo page

--- buildweb.sh
+++ buildweb.sh
@@ -1,11 +1,7 @@
-@echo "Clean existing repository"
 flutter clean
 
-@echo "Getting packages..."
 flutter pub get
 
-@echo "Generating the web folder..."
 flutter create . --platform web
 
-@echo "Building for web..."
-flutter build web --base-href asahi-note --release
+flutter build web --base-href /asahi-note/ --release
